9095646acd97199106f4c2367136168919d2d8ea
abbgavger <gaverstedt.lysander.geranis@abbgymnasiet.se>
2024-04-18 09:46:32 +0200

    Re-added ObjectDropped functionality

--- VR_RV/Assets/ScoringSystem/ScoreManager.cs
+++ VR_RV/Assets/ScoringSystem/ScoreManager.cs
@@ -4,44 +4,53 @@ using System;
 
 public class ScoreManager : MonoBehaviour
 {
-    private const int max_player_score = 50;
-    public int current_player_score {get; private set;}
+  private const int max_player_score = 100;
+  public int current_player_score { get; private set; }
 
-    public event Action<int> OnScoreUpdate;
+  public event Action<int> OnScoreUpdate;
+  public ObjectDropped objectDropped;
 
-    public static ScoreManager instance;
+  public static ScoreManager instance;
 
 
-    #region Singleton
-    void Awake(){
-      if(instance == null){
-        instance = this;
-        DontDestroyOnLoad(this);
-      }
-      else{
-        Destroy(this.gameObject);
-      }
-    }
-    #endregion
-
-    void Start()
+  #region Singleton
+  void Awake()
+  {
+    if (instance == null)
     {
-        current_player_score = max_player_score;
+      instance = this;
+      DontDestroyOnLoad(this);
     }
+    else
+    {
+      Destroy(this.gameObject);
+    }
+  }
+  #endregion
+  void Start()
+  {
+    current_player_score = max_player_score;
+  }
 
-    private void OnEnable(){
-      OnScoreUpdate += (int score_to_remove) =>{
-        Update_Score(score_to_remove);
-      };
-    }
+  private void OnEnable()
+  {
+    objectDropped.OnItemDropped += (int score_to_remove) =>
+    {
+      Update_Score(score_to_remove);
+    };
+  }
 
-    private void OnDisable(){
-      OnScoreUpdate -= (int score_to_remove) =>{
-        Update_Score(score_to_remove);
-      };
-    }
+  private void OnDisable()
+  {
+    objectDropped.OnItemDropped -= (int score_to_remove) =>
+    {
+      Update_Score(score_to_remove);
+    };
+  }
 
-    private void Update_Score(int score_to_remove){
-        current_player_score -= score_to_remove;
-    }
+  private void Update_Score(int score_to_remove)
+  {
+    current_player_score -= score_to_remove;
+    Debug.Log("Score: " + current_player_score);
+  }
 }
