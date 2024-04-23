981cbcfe809b03db091c81172fb28afb9d635b0d
Lucas Axberg <lucas.axberg@gmail.com>
2024-04-23 14:19:46 +0200

    Updated position of spawning sphere

--- VR_RV/Assets/Scenes/Other Scenes/disinfection-scene/mark_area.cs
+++ VR_RV/Assets/Scenes/Other Scenes/disinfection-scene/mark_area.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,23 +10,24 @@ public class mark_area : MonoBehaviour {
 
     private float half_z_axis_lenght;
     
-    private void Start () {
+    void Start () {
         half_z_axis_lenght = gameObject.transform.localScale.z / 2;
     }
-    private void Update() {
-        current_collisions = Physics.OverlapBox(transform.position, new Vector3(0f, 0f, transform.localScale.z / 2));
-        
+    void Update() {
+        current_collisions = Physics.OverlapSphere(transform.position, 1f);
+
         // Returns if the "rag" is not only colliding with the disinfectable area
         if (current_collisions.Length != 1) return;
         if (current_collisions[0].tag != "Disinfectable") return;
 
+        Debug.Log("Colldided");
         // Gets the sign of the difference in position
         float z_axis_ofset = current_collisions[0].transform.position.z - gameObject.transform.position.z;
         float z_axis_multiplier = Mathf.Sign(z_axis_ofset);
 
         // Adds sphere on collision position
         marker_spheres.Add(GameObject.CreatePrimitive(PrimitiveType.Sphere));
-        marker_spheres[marker_spheres.Count - 1].transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, half_z_axis_lenght *  z_axis_multiplier);
+        marker_spheres[marker_spheres.Count - 1].transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z + (half_z_axis_lenght *  z_axis_multiplier));
     }
 
 }
