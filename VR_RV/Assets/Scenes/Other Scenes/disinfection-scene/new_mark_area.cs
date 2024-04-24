1b09a316d971430639197ece1f2a58ac4c6b9d18
Lucas Axberg <lucas.axberg@gmail.com>
2024-04-24 10:49:25 +0200

    Implemented mesh instead of individual boxes

--- VR_RV/Assets/Scenes/Other Scenes/disinfection-scene/new_mark_area.cs
+++ VR_RV/Assets/Scenes/Other Scenes/disinfection-scene/new_mark_area.cs
@@ -7,6 +7,7 @@ using UnityEngine.Animations;
 public class new_mark_area : MonoBehaviour
 {
     [SerializeField] private int disinfectable_area_axis = 2;
+    [SerializeField] private GameObject disinfectable_area;
 
     private bool inside_sphere = false;
     private List<GameObject> current_markers = new List<GameObject>();
@@ -15,6 +16,16 @@ public class new_mark_area : MonoBehaviour
     private void Start()
     {
         mesh_object = new GameObject();
+        mesh_object.AddComponent<MeshFilter>();
+        mesh_object.AddComponent<MeshRenderer>();
+        mesh_object.AddComponent<MeshCollider>();
+        mesh_object.GetComponent<MeshCollider>().convex = true;
+        mesh_object.GetComponent<MeshCollider>().isTrigger = true;
+    }
+
+    private void Update()
+    {
+        mesh_object.transform.position = disinfectable_area.transform.position;
     }
     private void OnTriggerEnter(Collider collider)
     {
@@ -44,19 +55,23 @@ public class new_mark_area : MonoBehaviour
         Vector3 marker_position_vector = new Vector3(transform.position.x, transform.position.y, transform.position.z);
         marker_position_vector[disinfectable_area_axis] += axis_sign_multiplier * transform.localScale[disinfectable_area_axis] / 2;
 
-        // Creates a new marker, saves it to a list, and moves it to the correct position
-        current_markers.Add(GameObject.CreatePrimitive(PrimitiveType.Cube));
-        current_markers[current_markers.Count - 1].transform.position = marker_position_vector;
-        current_markers[current_markers.Count - 1].tag = "Marker_Cube";
-        current_markers[current_markers.Count - 1].GetComponent<BoxCollider>().isTrigger = true;
+        GameObject marker_cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        marker_cube.transform.position = marker_position_vector;
+        marker_cube.tag = "Marker_Cube";
+
+        Mesh new_mesh = AddObjectToMesh(marker_cube, mesh_object);
+        mesh_object.GetComponent<MeshFilter>().sharedMesh = new_mesh;
+        mesh_object.GetComponent<MeshCollider>().sharedMesh = new_mesh;
+
+        Destroy(marker_cube);
     }
 
-    private Mesh AddObjectToMesh(GameObject object_to_mesh, Mesh previous_mesh)
+    private Mesh AddObjectToMesh(GameObject object_to_mesh, GameObject previous_mesh_object)
     {
         
         Mesh combined_mesh = new Mesh();
 
-        MeshFilter[] mesh_filters = new MeshFilter[] { object_to_mesh.GetComponent<MeshFilter>(), previous_mesh.GetComponent<MeshFilter>() };
+        MeshFilter[] mesh_filters = new MeshFilter[] { object_to_mesh.GetComponent<MeshFilter>(), previous_mesh_object.GetComponent<MeshFilter>() };
         
         // Creates an array of combine instances for the gameObjects
         CombineInstance[] combiners = new CombineInstance[2];
