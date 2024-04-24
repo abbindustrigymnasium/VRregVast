f6abb37c5b9e2f8e805c6ef0aad3986ccf516621
Dennis Cejvan <66635133+tramvaj99@users.noreply.github.com>
2024-04-24 08:45:31 +0200

    Update AnimateHandController.cs
    Changed naming of variables to match guidelines

--- VR_RV/Assets/OculusBasicHands/AnimateHandController.cs
+++ VR_RV/Assets/OculusBasicHands/AnimateHandController.cs
@@ -6,8 +6,8 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(Animator))]
 public class AnimateHandController : MonoBehaviour
 {
-    public InputActionReference gripInputActionReference;
-    public InputActionReference triggerInputActionReference;
+    public InputActionReference grip_input_action_reference;
+    public InputActionReference trigger_input_action_reference;
 
     private Animator _hand_animator;
     private float _grip_value;
@@ -25,12 +25,12 @@ public class AnimateHandController : MonoBehaviour
     }
     private void Animate_Grip()
     {
-        _grip_value = gripInputActionReference.action.ReadValue<float>();
+        _grip_value = grip_input_action_reference.action.ReadValue<float>();
         _hand_animator.SetFloat("Grip", _grip_value);
     }
     private void Animate_Trigger()
     {
-        _trigger_value = triggerInputActionReference.action.ReadValue<float>();
+        _trigger_value = trigger_input_action_reference.action.ReadValue<float>();
         _hand_animator.SetFloat("Trigger", _trigger_value);
     }
 }
