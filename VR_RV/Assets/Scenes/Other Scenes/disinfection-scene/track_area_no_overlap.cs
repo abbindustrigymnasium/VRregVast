3ef08af2fffd8f7e118930900630229dbf4403c4
Lucas Axberg <lucas.axberg@gmail.com>
2024-04-30 13:23:09 +0200

    New concept for tracking

---
+++ VR_RV/Assets/Scenes/Other Scenes/disinfection-scene/track_area_no_overlap.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class track_area_no_overlap : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
