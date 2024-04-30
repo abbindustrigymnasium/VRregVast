5310f45fae159ce921a915c919b2589056d972b2
Lucas Axberg <lucas.axberg@gmail.com>
2024-04-30 14:58:00 +0200

    Spawns objects when only colliding with the right object

--- VR_RV/Assets/Scenes/Other Scenes/disinfection-scene/track_area_no_overlap.cs
+++ VR_RV/Assets/Scenes/Other Scenes/disinfection-scene/track_area_no_overlap.cs
@@ -8,8 +8,9 @@ public class track_area_no_overlap : MonoBehaviour
 {
     private void OnCollisionStay(Collision collision)
     {   
-        // Only runs if colliding with the right object
+        // Only runs if the rag is only colliding with the right object
         if (!collision.gameObject.CompareTag("Disinfectable")) return;
+        if (Physics.OverlapBox(transform.position, transform.localScale / 2, Quaternion.identity).Length > 2) return;
 
         // Create an array of contact points
         int contact_point_count = collision.contactCount;
@@ -20,6 +21,11 @@ public class track_area_no_overlap : MonoBehaviour
         Vector3 center = new Vector3();
         for(int i = 0; i < 3; i++) for (int j = 0; j < contact_point_count; j++) center[i] += contact_points[j][i];
         center /= contact_point_count;
+
+        GameObject ball = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        ball.transform.position = center;
+        ball.GetComponent<SphereCollider>().isTrigger = true;
+        ball.transform.localScale = new Vector3(2, 2, 2);
 
         Debug.Log(center);
     }
