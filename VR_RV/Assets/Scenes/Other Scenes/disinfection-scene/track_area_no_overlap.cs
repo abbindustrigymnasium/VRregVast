c2c20ed6686127a0307085e07b5f9107f8e6d5b5
Lucas Axberg <lucas.axberg@gmail.com>
2024-04-30 14:22:50 +0200

    Added code to track center of collision area

--- VR_RV/Assets/Scenes/Other Scenes/disinfection-scene/track_area_no_overlap.cs
+++ VR_RV/Assets/Scenes/Other Scenes/disinfection-scene/track_area_no_overlap.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using UnityEditor.Networking.PlayerConnection;
 using UnityEngine;
 
 public class track_area_no_overlap : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private void OnCollisionStay(Collision collision)
+    {   
+        // Only runs if colliding with the right object
+        if (!collision.gameObject.CompareTag("Disinfectable")) return;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Create an array of contact points
+        int contact_point_count = collision.contactCount;
+        Vector3[] contact_points = new Vector3[contact_point_count];
+        for(int i = 0; i < contact_point_count; i++) contact_points[i] = collision.contacts[i].point;
+
+        // Calculate center
+        Vector3 center = new Vector3();
+        for(int i = 0; i < 3; i++) for (int j = 0; j < contact_point_count; j++) center[i] += contact_points[j][i];
+        center /= contact_point_count;
+
+        Debug.Log(center);
     }
 }
