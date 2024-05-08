367e8385505411d28a1e40491d3e9baba0113077
ABB22OllOst <olle.ostman@abbgymnasiet.se>
2024-05-08 13:37:48 +0200

    Added some comments to MenuControlls

--- VR_RV/Assets/Scenes/Other Scenes/Menu Scene/Scripts/MenuControlls.cs
+++ VR_RV/Assets/Scenes/Other Scenes/Menu Scene/Scripts/MenuControlls.cs
@@ -5,18 +5,22 @@ using TMPro;
 
 public class MenuControlls : MonoBehaviour
 {
+    // Text object you want to change the text on
     public TMP_Text text_gameobject;
 
+    // Changes a text to say "Vill du starta Övning?"
     public void Övning()
     {
         text_gameobject.text = "Vill du starta Övning?";
     }
 
+    // Changes a text to say "Vill du starta Prov?"
     public void Prov()
     {
         text_gameobject.text = "Vill du starta Prov?";
     }
 
+    // Closes down the game
     public void Quit()
     {
         Application.Quit();
