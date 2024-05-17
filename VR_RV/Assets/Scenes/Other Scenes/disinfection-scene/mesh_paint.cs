b376df9f1dc6ba83e45671d8839536610db08959
Lucas Axberg <lucas.axberg@gmail.com>
2024-05-17 14:39:20 +0200

    Removed unused function

--- VR_RV/Assets/Scenes/Other Scenes/disinfection-scene/mesh_paint.cs
+++ VR_RV/Assets/Scenes/Other Scenes/disinfection-scene/mesh_paint.cs
@@ -48,8 +48,5 @@ public class mesh_paint : MonoBehaviour
         }
         dirt_mask.Apply();
     }
-    public float Remap(float value, float from1, float to1, float from2, float to2)
-    {
-        return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
-    }
+    
 }
