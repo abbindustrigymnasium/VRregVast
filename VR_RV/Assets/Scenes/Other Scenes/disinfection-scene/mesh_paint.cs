8c2d0904c9ac0a40baaeab729159b9378f4ad592
Lucas Axberg <lucas.axberg@gmail.com>
2024-05-17 14:38:31 +0200

    Script now edits the mask on collission

--- VR_RV/Assets/Scenes/Other Scenes/disinfection-scene/mesh_paint.cs
+++ VR_RV/Assets/Scenes/Other Scenes/disinfection-scene/mesh_paint.cs
@@ -1,15 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class mesh_paint : MonoBehaviour
 {
 
-    [SerializeField] private Texture2D base_layer;
-    // Start is called before the first frame update
+    [SerializeField] private Texture2D dirt_mask_base;
+    private Texture2D dirt_mask;
+    private GameObject disinfectable_area;
+    private Material disinfectable_area_material;
+
     void Start()
     {
-        
+        disinfectable_area = GameObject.FindGameObjectWithTag("Disinfectable");
+        disinfectable_area_material = disinfectable_area.GetComponent<Renderer>().material;
+
+        dirt_mask = new Texture2D(dirt_mask_base.width, dirt_mask_base.height);
+        dirt_mask.SetPixels(dirt_mask_base.GetPixels());
+        dirt_mask.Apply();
+        disinfectable_area_material.SetTexture("_dirt_mask", dirt_mask);
+
     }
 
     // Update is called once per frame
@@ -24,8 +35,21 @@ public class mesh_paint : MonoBehaviour
         float x_axis_scale = transform.localScale.x / collision.transform.localScale.x;
         float y_axis_scale = transform.localScale.y / collision.transform.localScale.y;
 
-        int x_width = (int)(x_axis_scale * base_layer.width);
-        int y_height = (int)(y_axis_scale * base_layer.height);
+        int x_width = (int)(x_axis_scale * dirt_mask_base.width);
+        int y_height = (int)(y_axis_scale * dirt_mask_base.height);
 
+
+        for (int i = 0; i < x_width; i++)
+        {
+            for(int j = 0; j < y_height; j++)
+            {
+                dirt_mask.SetPixel(i,j,Color.black); 
+            }
+        }
+        dirt_mask.Apply();
+    }
+    public float Remap(float value, float from1, float to1, float from2, float to2)
+    {
+        return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
     }
 }
