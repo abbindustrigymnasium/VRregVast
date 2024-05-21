9134de3c07b1f47b353f06daa9f7e360a2237ffc
Lucas Axberg <lucas.axberg@gmail.com>
2024-05-21 13:40:24 +0200

    Implemented the feature of removing pixels from the mesh, based on collision position

--- VR_RV/Assets/Scenes/Other Scenes/disinfection-scene/mesh_paint.cs
+++ VR_RV/Assets/Scenes/Other Scenes/disinfection-scene/mesh_paint.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class mesh_paint : MonoBehaviour
 {
@@ -28,7 +29,7 @@ public class mesh_paint : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        relative_position = transform.position - disinfectable_area.transform.position;
+        
     }
 
     private void OnCollisionStay(Collision collision)
@@ -37,18 +38,27 @@ public class mesh_paint : MonoBehaviour
         float x_axis_scale = transform.localScale.x / collision.transform.localScale.x;
         float y_axis_scale = transform.localScale.y / collision.transform.localScale.y;
 
-        int x_width = (int)(x_axis_scale * dirt_mask_base.width);
-        int y_height = (int)(y_axis_scale * dirt_mask_base.height);
+        // Convert the relative size to pixel size on layer mask
+        int pixel_width = (int)(x_axis_scale * dirt_mask_base.width);
+        int pixel_height = (int)(y_axis_scale * dirt_mask_base.height);
+
+        relative_position = transform.position - collision.gameObject.transform.position;
+
+        // Calculate the start position on the layer mask in pixels
+        int[] pixel_start = new int[2];
+        pixel_start[0] = (int) ((relative_position.x + collision.gameObject.transform.localScale.x / 2) * pixel_width - transform.localScale.x * pixel_width / 2);
+        pixel_start[1] = (int) ((relative_position.y + collision.gameObject.transform.localScale.y / 2) * pixel_height - transform.localScale.y * pixel_height / 2);
 
 
-        for (int i = 0; i < x_width; i++)
+        for (int i = 0; i < pixel_width; i++)
         {
-            for(int j = 0; j < y_height; j++)
+            for(int j = 0; j < pixel_height; j++)
             {
-                dirt_mask.SetPixel(i,j,Color.black); 
+                dirt_mask.SetPixel(pixel_start[0] + i,pixel_start[1] + j,Color.black); 
             }
         }
         dirt_mask.Apply();
     }
+    
     
 }
