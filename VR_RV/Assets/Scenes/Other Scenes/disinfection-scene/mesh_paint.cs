d83f018059ecb4a264100bca98c50c11b9c0a863
Lucas Axberg <lucas.axberg@gmail.com>
2024-05-21 14:12:57 +0200

    Added code to calculate percentage of colored pixles

--- VR_RV/Assets/Scenes/Other Scenes/disinfection-scene/mesh_paint.cs
+++ VR_RV/Assets/Scenes/Other Scenes/disinfection-scene/mesh_paint.cs
@@ -11,13 +11,15 @@ public class mesh_paint : MonoBehaviour
     private Texture2D dirt_mask;
     private GameObject disinfectable_area;
     private Material disinfectable_area_material;
-
-    private Vector3 relative_position;
+    private Color[] default_state_pixel_array;
+    private float percentage_colored;
 
     void Start()
     {
         disinfectable_area = GameObject.FindGameObjectWithTag("Disinfectable");
         disinfectable_area_material = disinfectable_area.GetComponent<Renderer>().material;
+
+        default_state_pixel_array = dirt_mask_base.GetPixels();
 
         dirt_mask = new Texture2D(dirt_mask_base.width, dirt_mask_base.height);
         dirt_mask.SetPixels(dirt_mask_base.GetPixels());
@@ -29,27 +31,44 @@ public class mesh_paint : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        int colored_pixles = 0;
         
+        // Get the color of each pixel of the edited layer mask 
+        Color[] comparable_pixel_array = dirt_mask.GetPixels();
+     
+        // Compare the colors of the unedited and edited layer masks
+        for (int i = 0; i < default_state_pixel_array.Length; i++)
+        {
+            // Increase the colored_pixels variables if the colors isn't the same
+            if (comparable_pixel_array[i] != default_state_pixel_array[i]) colored_pixles++;
+        }
+
+        // Calculate the percentage of colored pixles
+        percentage_colored = colored_pixles / default_state_pixel_array.Length;
+        Debug.Log(colored_pixles);
     }
 
     private void OnCollisionStay(Collision collision)
     {
         if(!collision.gameObject.CompareTag("Disinfectable")) return;
+
+        // Calculate the relative size of the rag compared to the disinfectable area
         float x_axis_scale = transform.localScale.x / collision.transform.localScale.x;
         float y_axis_scale = transform.localScale.y / collision.transform.localScale.y;
 
-        // Convert the relative size to pixel size on layer mask
+        // Calculate the width and height of the rag in pixles
         int pixel_width = (int)(x_axis_scale * dirt_mask_base.width);
         int pixel_height = (int)(y_axis_scale * dirt_mask_base.height);
 
-        relative_position = transform.position - collision.gameObject.transform.position;
+        // Calculate the relative position of the rag to the disinfectable object
+        Vector3 relative_position = transform.position - collision.gameObject.transform.position;
 
-        // Calculate the start position on the layer mask in pixels
+        // Calculate the start position to color on the layer mask in pixels
         int[] pixel_start = new int[2];
         pixel_start[0] = (int) ((relative_position.x + collision.gameObject.transform.localScale.x / 2) * pixel_width - transform.localScale.x * pixel_width / 2);
         pixel_start[1] = (int) ((relative_position.y + collision.gameObject.transform.localScale.y / 2) * pixel_height - transform.localScale.y * pixel_height / 2);
 
-
+        // Set all the pixles within the width of the rag to black, starting in the bottom left
         for (int i = 0; i < pixel_width; i++)
         {
             for(int j = 0; j < pixel_height; j++)
@@ -57,7 +76,10 @@ public class mesh_paint : MonoBehaviour
                 dirt_mask.SetPixel(pixel_start[0] + i,pixel_start[1] + j,Color.black); 
             }
         }
+
+        // Apply the changes
         dirt_mask.Apply();
+        
     }
     
     
