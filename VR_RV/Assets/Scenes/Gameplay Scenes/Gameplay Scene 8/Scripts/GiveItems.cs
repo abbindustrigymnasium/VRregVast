73bf720d9808486c7837012ff0e67819e0d81cbd
Simon Meier <116065827+abbsimmei@users.noreply.github.com>
2024-05-21 13:53:07 +0200

    Removes Objects in VR now
    Yippie

--- VR_RV/Assets/Scenes/Gameplay Scenes/Gameplay Scene 8/Scripts/GiveItems.cs
+++ VR_RV/Assets/Scenes/Gameplay Scenes/Gameplay Scene 8/Scripts/GiveItems.cs
@@ -7,25 +7,51 @@ public class GiveItems : MonoBehaviour
 {
     public Transform Box1;
     public GameObject Box2;
-    private int Request = 2;
+    private int Request = 0;
     private GameObject child;
 
-    public void Update()
+    void Start()
+    {
+        ChildUpdate();
+        if (Box1 == null)
+        {
+            Debug.LogError("Box1 is not assigned in the inspector.");
+            return;
+        }
+
+        if (Box1.childCount == 0)
+        {
+            Debug.LogError("Box1 has no children.");
+            return;
+        }
+
+        for (int i = 0; i < Box1.childCount; i++)
+        {
+            Debug.Log(Box1.GetChild(i).gameObject);
+        }
+
+        Debug.Log("Box1 is assigned and has " + Box1.childCount + " children.");
+    }
+
+
+    public void ChildUpdate()
     {
         child = Box1.GetChild(Request).gameObject;
     }
 
     public void On_Select_Entered_Snap(SelectEnterEventArgs args)
     {
-        if (GameObject.ReferenceEquals(args.interactableObject, Box1.GetChild(Request).gameObject.GetComponent<XRGrabInteractable>()))
+        if (GameObject.ReferenceEquals(args.interactableObject, child.GetComponent<XRGrabInteractable>()))
         {
-            GetComponent<MeshRenderer>().material.color = Color.green;
+            //GetComponent<MeshRenderer>().material.color = Color.green;
             // TODO: update the score here;
             child.SetActive(false);
-            Request--;
+            Request++;
+            ChildUpdate();
         }
         else
         {
+            Debug.Log("Wrong Tool");
             // GetComponent<MeshRenderer>().material.color = Color.red;
         }
     }
