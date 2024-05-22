18f5e29e84d5e35642bed95daa00eb4a01d4bf94
Lucas Axberg <lucas.axberg@gmail.com>
2024-05-22 10:42:41 +0200

    Updated script to use Color32 and not paint pixels outside the limits

--- VR_RV/Assets/Scenes/Other Scenes/disinfection-scene/mesh_paint.cs
+++ VR_RV/Assets/Scenes/Other Scenes/disinfection-scene/mesh_paint.cs
@@ -9,7 +9,7 @@ public class mesh_paint : MonoBehaviour
 
     [SerializeField] private Texture2D layer_mask_default;
     private Texture2D editable_layer_mask;
-    private Color[] default_state_pixel_array;
+    private Color32[] default_state_pixel_array;
     private float percentage_colored;
 
     void Start()
@@ -19,8 +19,8 @@ public class mesh_paint : MonoBehaviour
 
         editable_layer_mask = new Texture2D(layer_mask_default.width, layer_mask_default.height);
 
-        default_state_pixel_array = layer_mask_default.GetPixels();
-        editable_layer_mask.SetPixels(layer_mask_default.GetPixels());
+        default_state_pixel_array = layer_mask_default.GetPixels32();
+        editable_layer_mask.SetPixels32(default_state_pixel_array);
         editable_layer_mask.Apply();
 
         disinfectable_area_material.SetTexture("_layer_mask", editable_layer_mask);
@@ -30,22 +30,21 @@ public class mesh_paint : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /*
+        
         int colored_pixles = 0;
         
         // Get the color of each pixel of the edited layer mask 
-        Color[] comparable_pixel_array = editable_layer_mask.GetPixels();
+        Color32[] comparable_pixel_array = editable_layer_mask.GetPixels32();
      
         // Compare the colors of the unedited and edited layer masks
         for (int i = 0; i < default_state_pixel_array.Length; i++)
         {
             // Increase the colored_pixels variables if the colors isn't the same
-            if (comparable_pixel_array[i] != default_state_pixel_array[i]) colored_pixles++;
+            if (comparable_pixel_array[i].g != default_state_pixel_array[i].g) colored_pixles++;
         }
-        */
+        
         // Calculate the percentage of colored pixles
-        //percentage_colored = colored_pixles / default_state_pixel_array.Length;
-        //Debug.Log(colored_pixles);
+        percentage_colored = (colored_pixles * 100 / default_state_pixel_array.Length);
     }
 
     private void OnCollisionStay(Collision collision)
@@ -71,9 +70,20 @@ public class mesh_paint : MonoBehaviour
         // Set all the pixles within the width of the rag to black, starting in the bottom left
         for (int i = 0; i < pixel_width; i++)
         {
-            for(int j = 0; j < pixel_height; j++)
+            // Calculate the x-axis position of the pixel to paint
+            int x_pixel_position = pixel_start[0] + i;
+            if (x_pixel_position < 0) x_pixel_position = 0;
+            if (x_pixel_position >= layer_mask_default.width) x_pixel_position = layer_mask_default.width - 1;
+
+
+            for (int j = 0; j < pixel_height; j++)
             {
-                editable_layer_mask.SetPixel(pixel_start[0] + i,pixel_start[1] + j,Color.black); 
+                // Calculate the y-axis position of the pixel to paint
+                int y_pixel_position = pixel_start[1] + j;
+                if (y_pixel_position < 0) y_pixel_position = 0;
+                if (y_pixel_position >= layer_mask_default.height) y_pixel_position = layer_mask_default.height - 1;
+
+                editable_layer_mask.SetPixel(x_pixel_position, y_pixel_position, Color.black); 
             }
         }
 
