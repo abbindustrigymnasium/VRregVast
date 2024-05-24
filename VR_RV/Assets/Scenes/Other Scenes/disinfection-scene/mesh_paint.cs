72ad476f823a6719e46c21154919ad4b332a21c0
Lucas Axberg <lucas.axberg@gmail.com>
2024-05-24 08:19:48 +0200

    Removed unused namespaces

--- VR_RV/Assets/Scenes/Other Scenes/disinfection-scene/mesh_paint.cs
+++ VR_RV/Assets/Scenes/Other Scenes/disinfection-scene/mesh_paint.cs
@@ -1,9 +1,6 @@
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class mesh_paint : MonoBehaviour
 {
