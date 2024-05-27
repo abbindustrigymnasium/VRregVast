de7e9b97f348f9c2741db8779ed4ea2b25737284
ABB22denfor <drexman45@gmail.com>
2024-05-27 12:42:03 +0200

    Added test code for fixing bug

--- VR_RV/Assets/Scenes/Gameplay Scenes/Bare Bones/ToolRandomizer/ToolRandomizer.cs
+++ VR_RV/Assets/Scenes/Gameplay Scenes/Bare Bones/ToolRandomizer/ToolRandomizer.cs
@@ -13,6 +13,8 @@ public class ToolRandomizer : MonoBehaviour
 
     private float timer = 0f;
 
+    public GameObject test_parent;
+
     void Awake(){
       saved_spawnpoints = tool_spawnpoints_list.ToArray();
     }
@@ -31,16 +33,28 @@ public class ToolRandomizer : MonoBehaviour
     // Randomize spawn positions of items with specific tag
     void Randomize_Items(string tag){
       all_tools = GameObject.FindGameObjectsWithTag(tag);
+      test_parent.transform.position = new Vector3(0.321f, 1.5f, -2.2898f);
 
-      foreach(GameObject tool in all_tools){
+      /* foreach(GameObject tool in all_tools){
         int index = Random.Range(0, tool_spawnpoints_list.Count);
+
+        Transform[] transform_array = tool.GetComponentsInChildren<Transform>();
+        List<GameObject> children = new();
+
+        foreach(Transform transform in transform_array){
+          children.Add(transform.gameObject);
+          Debug.Log(transform.gameObject);
+        }
+        foreach(GameObject child in children){
+          child.transform.position = tool_spawnpoints_list[index];
+        }
 
         // Set tool position to random position from list
         tool.transform.position = tool_spawnpoints_list[index];
 
         // Remove position from list to avoid tools spawning on the same point
         tool_spawnpoints_list.Remove(tool_spawnpoints_list[index]);
-      }
+      }*/
 
     }
 }
